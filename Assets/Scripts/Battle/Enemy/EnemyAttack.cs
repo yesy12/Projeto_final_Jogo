a0558bf660037414fdf066b0684b082205061aac
Alison <alisonvieira.av29@gmail.com>
2020-02-18 17:32:33 -0300

    criado enemyAttack

--- Assets/Scripts/Battle/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Battle/Enemy/EnemyAttack.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAttack : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class EnemyAttack : MonoBehaviour{
+
+    public Transform person;
+    public float velocityEnemy;
+    float zeroF;
+
+    void Start(){
+        zeroF = 0f;
+        velocityEnemy = 10;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        followPlayer();
+    }
+
+    void followPlayer(){
+        transform.LookAt(person.position);
+        transform.Translate(zeroF,zeroF, velocityEnemy * Time.deltaTime);
     }
 }
